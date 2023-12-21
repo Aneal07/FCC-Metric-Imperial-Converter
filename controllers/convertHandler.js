c37f4d91a8aca944043e6e37533ee35ef7f5775f
Aneal07 <anilmaharjan07@outlook.com>
2023-12-21 14:52:26 +0000

    Added api routes and numberspiltter

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,23 +1,25 @@
+function numberStringSplitter (input) {
+  let number = input.match(/[.\d\/]+/g) || ["1"]
+  let string = input.match(/[a-zA-Z]+/g)[0]
+
+  return [number[0], string]
+}
+function checkDiv(possibleFraction) {
+  let nums = possibleFraction.split("/")
+  if(nums.length > 2) {
+    return false
+  }
+  return nums
+}
+
+
 function ConvertHandler() {
   
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
+ 
   
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
 
+  
+ 
   this.spellOutUnit = function(unit) {
     let result;
     
